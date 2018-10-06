82661aeed38e63eeecd3b68b25848de17b6c5292
VasilchenkoSergei <chainsaw86@mail.ru>
2018-10-07 00:02:59 +0300

    slider work on

--- src/js/script.js
+++ src/js/script.js
@@ -6,62 +6,107 @@ $(document).ready(function(){
   // $(".page-header").toggle();
   //   });
 
+$('.cakebox-list').owlCarousel ({
+  dots: true,
+  center: true,
+  nav: false,
+  dots: true,
+  responsive : {
+    0 : {
+        items : 1,
+    },
+    767 : {
+        items : 1,
+        dots: false,
+        nav: true,
+        navText: ["<img src='img/icons/left-icon.svg'>", "<img src='img/icons/right-icon.svg'>"],
+    }
+  }
+});
 
+$('.sizebox-list').owlCarousel ({
+  loop:true,
+  margin:10,
+  responsive : {
+      0 : {
+          items : 1,
 
-  $('.cakebox-list').slick({
-    dots: false,
-    arrows: false,
-    centerMode: true,
-    variableWidth: true,
-    infinite: true,
-    responsive: [
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToScroll: 1,
-          slidesToShow: 1
-        }
       },
-      {
-        breakpoint: 1199,
-        settings: {
-          slidesToScroll: 1,
-          slidesToShow: 1
-        }
+      767 : {
+          items : 4,
+          loop : false,
+          mouseDrag: false,
+          autoHeight:false,
       }
-    ]
-  });
+  }
+});
 
-  $('.sizebox-list').slick({
-    dots: true,
-    arrows: false,
-    centerMode: true,
-    variableWidth: true,
-    infinite: true,
-    responsive: [
-      {
-        breakpoint: 767,
-        settings: 'unslick'
-      }
-    ]
-  });
 
-    $('.appearbox-list').slick({
-    dots: true,
-    arrows: false,
-    infinite: true,
-    slidesToShow: 1,
-    centerMode: true,
-    responsive: [
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
+
+
+$(window).resize(function() {
+    if ( $(window).width() > 767 ) {
+        $('.sizebox-list').removeClass();
         }
-      }
-    ]
-  });
+    else {
+        $('.sizebox-list').addClass();
+        }
+    });
+
+  // $('.cakebox-list').slick({
+  //   dots: false,
+  //   arrows: false,
+  //   centerMode: true,
+  //   variableWidth: true,
+  //   infinite: true,
+  //   responsive: [
+  //     {
+  //       breakpoint: 767,
+  //       settings: {
+  //         slidesToScroll: 1,
+  //         slidesToShow: 1
+  //       }
+  //     },
+  //     {
+  //       breakpoint: 1199,
+  //       settings: {
+  //         slidesToScroll: 1,
+  //         slidesToShow: 1
+  //       }
+  //     }
+  //   ]
+  // });
+
+  // $('.sizebox-list').slick({
+  //   dots: true,
+  //   arrows: false,
+  //   centerMode: true,
+  //   variableWidth: true,
+  //   infinite: true,
+  //   responsive: [
+  //     {
+  //       breakpoint: 767,
+  //       settings: 'unslick'
+  //     }
+  //   ]
+  // });
+
+  //   $('.appearbox-list').slick({
+  //   dots: true,
+  //   arrows: false,
+  //   infinite: true,
+  //   slidesToShow: 1,
+  //   centerMode: true,
+  //   responsive: [
+  //     {
+  //       breakpoint: 767,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 2
+  //       }
+  //     }
+  //   ]
+  // });
 
   // $('.coach-photos__list').slick({
   //   arrows: false,
